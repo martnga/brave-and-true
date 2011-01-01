80bafbf9336b193e5dd7daebfc659b8ea2c7e32b
martnga <mtnjoroge@gmail.com>
2011-01-01 14:15:13 +0300

    Naming threads

---
+++ src/Example.java
@@ -0,0 +1,24 @@
+
+/*
+This is a simple Java program.
+Call this file "Example.java".
+*/
+class Example {
+// Your program begins with a call to main().
+
+public static void main(String args[])  {
+	Thread t = Thread.currentThread();
+	System.out.println("Current thread: " + t);
+	// change the name of the thread
+	t.setName("My Thread");
+	System.out.println("After name change: " + t);
+	try {
+	for(int n = 5; n > 0; n--) {
+	System.out.println(n);
+	Thread.sleep(1000);
+	}
+	} catch (InterruptedException e) {
+	System.out.println("Main thread interrupted");
+	}
+	}
+}
