bf711cb132119b28727a9b6070b7b75738eff61d
martnga <mtnjoroge@gmail.com>
2015-11-18 02:56:41 +0300

    Generics Syntax

--- src/Example.java
+++ src/Example.java
@@ -1,31 +1,31 @@
-
-/*
-This is a simple Java program.
-Call this file "Example.java".
-*/
-class Example implements Runnable{
-// Your program begins with a call to main().
-	Thread t ;
-	
-	Example() {
-		// Create a new, Second Thread
-		t = new Thread(this, "Demo Thread");
-		System.out.println("Second Thread: " + t);
-		t.start();
-	}
-
-@Override
-public void run() {
-	// TODO Auto-generated method stub
-	try {
-		for(int i = 5; i > 0; i--){
-		  System.out.println("Second Thread: " + i);
-		  Thread.sleep(500);
-		}
-	}catch (InterruptedException e){
-		System.out.println("Child interrupted. " + e);
-	}
-	System.out.println("Exiting child Thread");
-}
-}
-
+//
+///*
+//This is a simple Java program.
+//Call this file "Example.java".
+//*/
+//class Example implements Runnable{
+//// Your program begins with a call to main().
+//	Thread t ;
+//	
+//	Example() {
+//		// Create a new, Second Thread
+//		t = new Thread(this, "Demo Thread");
+//		System.out.println("Second Thread: " + t);
+//		t.start();
+//	}
+//
+//@Override
+//public void run() {
+//	// TODO Auto-generated method stub
+//	try {
+//		for(int i = 5; i > 0; i--){
+//		  System.out.println("Second Thread: " + i);
+//		  Thread.sleep(500);
+//		}
+//	}catch (InterruptedException e){
+//		System.out.println("Child interrupted. " + e);
+//	}
+//	System.out.println("Exiting child Thread");
+//}
+//}
+//
--- src/ThreadDemo.java
+++ src/ThreadDemo.java
@@ -1,18 +1,18 @@
-
-public class ThreadDemo {
-	
-	public static void main(String args[])  {
-		new Example();
-		
-		try {
-			for(int i = 5; i > 0; i--){
-				System.out.println("Main Thread: " + i);
-				Thread.sleep(1000);
-			}
-		}catch(InterruptedException e){
-			System.out.println("MainThread interrupted. " + e);
-		}
-		  System.out.println("Main Thread terminating");
-		}
-
-}
+//
+//public class ThreadDemo {
+//	
+//	public static void main(String args[])  {
+//		new Example();
+//		
+//		try {
+//			for(int i = 5; i > 0; i--){
+//				System.out.println("Main Thread: " + i);
+//				Thread.sleep(1000);
+//			}
+//		}catch(InterruptedException e){
+//			System.out.println("MainThread interrupted. " + e);
+//		}
+//		  System.out.println("Main Thread terminating");
+//		}
+//
+//}
